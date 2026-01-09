f27584e01cc748e4575f98764357d419264eb1a3
James <jamese.spaceworld@gmail.com>
2026-01-09 20:42:21 +0000

    feat: add branch folder and define the columns for the branch model in the folder's models.py file

---
+++ src/objects/branch/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column,String,DateTime,ForeignKey
+from src.models.base import UUIDModel, TimestampedModel
+from uuid import UUID
+from sqlalchemy.dialects.postgresql import UUID as pgUUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+#Defines a pharmacy branch model
+class Branch(UUIDModel, TimestampedModel):
+    pharmacy_id: Mapped[UUID] = mapped_column(ForeignKey, nullable=False, index=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
