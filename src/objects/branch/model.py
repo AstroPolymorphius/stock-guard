40859abd75a6150b0b28a0081b5af39fdd4ec873
James <jamese.spaceworld@gmail.com>
2026-01-12 16:24:59 +0000

    feat: define relationship between pharmacy and branch models in their model.py file and import pharmacy and branch in the objects folder's __init__.py file

--- src/objects/branch/model.py
+++ src/objects/branch/model.py
@@ -1,11 +1,17 @@
+from typing import TYPE_CHECKING
 from sqlalchemy import Column,String,DateTime,ForeignKey
 from src.models.base import UUIDModel, TimestampedModel
 from uuid import UUID
 from sqlalchemy.dialects.postgresql import UUID as pgUUID
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+if TYPE_CHECKING:
+    from src.objects.pharmacy.model import Pharmacy
 #Defines a pharmacy branch model
 class Branch(UUIDModel, TimestampedModel):
-    pharmacy_id: Mapped[UUID] = mapped_column(ForeignKey, nullable=False, index=True)
+    __table__ = "branches"
+    pharmacy_id: Mapped[UUID] = mapped_column(ForeignKey("pharmacies.id"), nullable=False, index=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
     location: Mapped[str] = mapped_column(String, nullable=False)
+    ##Establishing the relationship between pharmacy and branch models here in the child model
+    pharmacy: Mapped["Pharmacy"] = relationship("Pharmacy", back_populates="branches")
--- src/objects/pharmacy/model.py
+++ src/objects/pharmacy/model.py
@@ -1,10 +1,15 @@
+from typing import List,TYPE_CHECKING
 from sqlalchemy import Column,String,Integer,DateTime,ForeignKey
 from src.models.base import UUIDModel, TimestampedModel
 from uuid import UUID
 from sqlalchemy.dialects.postgresql import UUID as pgUUID
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+if TYPE_CHECKING:
+    from src.objects.branch.model import Branch
 #Defines a pharmacy model
 class Pharmacy(UUIDModel, TimestampedModel):
-    __table__ = "Pharmacies"
+    __table__ = "pharmacies"
     name: Mapped[str] = mapped_column(String, nullable=False)
+    #Establishing relationship between pharmacy and branch models here in the parent model
+    branches: Mapped[List["Branch"]] = relationship(back_populates="pharmacy")
